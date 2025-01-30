8577930610b6aa8814b69f0b38443b3b6412d101
Fatih Tuğrul Bakkal <137514988+fatihtugrulbakkal@users.noreply.github.com>
2025-01-30 13:50:11 +0300

    Create header.php

---
+++ panel/includes/header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <a href="dashboard.php">GiyimButik Admin</a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="urunler.php">Ürünler</a></li>
+                <li><a href="kullanicilar.php">Kullanıcılar</a></li>
+                <li><a href="siparisler.php">Siparişler</a></li>
+                <li><a href="kategoriler.php">Kategoriler</a></li>
+                <li><a href="yorumlar.php">Yorumlar</a></li>
+                <li><a href="raporlar.php">Raporlar</a></li>
+                <li><a href="ayarlar.php">Ayarlar</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
